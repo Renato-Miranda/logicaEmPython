453c5cda5b747743ae40951aec133c4511b65de5
Renato <renatomna@gmail.com>
2024-11-12 15:11:00 -0300

    feat: :art: Exercício Extra 3 - contando palavras em um texto.

--- dia6ListasTuplas.py
+++ dia6ListasTuplas.py
@@ -99,32 +99,48 @@
 
 #2 Analisador de notas
 
-notas = []
+# notas = []
 
-while True:
-    entrada = input("Digite uma nota (ou 'sair' para finalizar): ")
-    if entrada.lower() == 'sair':
-        break
+# while True:
+#     entrada = input("Digite uma nota (ou 'sair' para finalizar): ")
+#     if entrada.lower() == 'sair':
+#         break
+#     else:
+#         try:
+#             nota = float(entrada)
+#             if 0 <= nota <= 10:
+#                 notas.append(nota)
+#             else:
+#                 print("Nota inválida. Digite um valor entre 0 e 10.")
+#         except ValueError:
+#             print("Entrada inválida. pro favor, digite um nímero.")
+
+# if notas:
+#     maior_nota = max(notas)
+#     menor_nota = min(notas)
+#     media = sum(notas)
+#     notas_acima_media = [nota for nota in notas if nota > media]
+
+#     print("\nResultados:")
+#     print("Maior nota:", maior_nota)
+#     print("Menor nota:", menor_nota)
+#     print("Média da turma:", media)
+#     print("Notas acima da média:", notas_acima_media)
+# else:
+#     print("Nehuma nota foi inserida")
+
+#3 Contando Palavras em um texto
+
+texto = input("Digite um texto: ").lower()
+palavras = texto.split()
+contagem_palavras = {}
+
+for palavra in palavras:
+    if palavra in contagem_palavras:
+        contagem_palavras[palavra] += 1
     else:
-        try:
-            nota = float(entrada)
-            if 0 <= nota <= 10:
-                notas.append(nota)
-            else:
-                print("Nota inválida. Digite um valor entre 0 e 10.")
-        except ValueError:
-            print("Entrada inválida. pro favor, digite um nímero.")
+        contagem_palavras[palavra] = 1
 
-if notas:
-    maior_nota = max(notas)
-    menor_nota = min(notas)
-    media = sum(notas)
-    notas_acima_media = [nota for nota in notas if nota > media]
-
-    print("\nResultados:")
-    print("Maior nota:", maior_nota)
-    print("Menor nota:", menor_nota)
-    print("Média da turma:", media)
-    print("Notas acima da média:", notas_acima_media)
-else:
-    print("Nehuma nota foi inserida")
+print("\nContagem de palavras:")
+for palavra, contagem in contagem_palavras.items():
+    print(f"A palavra '{palavra}' aparece {contagem} vez(es)")
