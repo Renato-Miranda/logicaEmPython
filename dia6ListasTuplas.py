3f5914c49c5b85e00b805747440a7a1156527e43
Renato <renatomna@gmail.com>
2024-11-12 14:49:59 -0300

    feat: :art: Exercício Extra 1 - Lista de Tarefas

--- dia6ListasTuplas.py
+++ dia6ListasTuplas.py
@@ -51,13 +51,49 @@
 
 #5 Trabalhando com tuplas
 
-meses = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-"Julho", "Agosto", "Setembro", "Outubro", "Novembro",
-"Dezembro")
+# meses = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+# "Julho", "Agosto", "Setembro", "Outubro", "Novembro",
+# "Dezembro")
 
-numero_mes = int(input("Digite um número entre 1 e 12: "))
+# numero_mes = int(input("Digite um número entre 1 e 12: "))
 
-if 1 <= numero_mes <= 12:
-    print(f"O mês correspondente é {meses[numero_mes - 1]}.")
-else:
-    print("Números inválido. por favor, digite um número entre 1 e 12.")
+# if 1 <= numero_mes <= 12:
+#     print(f"O mês correspondente é {meses[numero_mes - 1]}.")
+# else:
+#     print("Números inválido. por favor, digite um número entre 1 e 12.")
+
+## Desafios Extras
+#1 Lista de Tarefas
+
+tarefas = []
+
+while True:
+    print("\nMenu de Tarefas:")
+    print("1. Adicionar tarefa")
+    print("2. Remover tarefa")
+    print("3. Listar tarefas")
+    print("4. Sair")
+
+    opcao = input("Escolhas uma opção: ")
+
+    if opcao == "1": 
+        tarefa = input("Digite a tarefa a ser adicionada: ")
+        tarefas.append(tarefa)
+        print("Tarefa adicionada com sucesso.")
+    elif opcao == "2":
+        tarefa = input("Digite uma tarefa a ser removida: ")
+        if tarefa in tarefas:
+            tarefas.remove(tarefa)
+            print("Tarefa removida com sucesso.")
+        else:
+            print("TArefa não encontrada")
+    elif opcao == "3":
+        print("\nLista de Tarefas:")
+        for idx, tarefa in enumerate(tarefas, start=1):
+            print(f"{idx}. {tarefa}")
+    elif opcao == "4":
+        print("Saindo do programa.")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
+
