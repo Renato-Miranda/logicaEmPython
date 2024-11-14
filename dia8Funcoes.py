3fb96477bcb7bb39461d8f1c6c8dea01cb5b44aa
Renato <renatomna@gmail.com>
2024-11-14 11:10:33 -0300

    feat: :art: Exercício com função que verifica se um numero é primo.

--- dia8Funcoes.py
+++ dia8Funcoes.py
@@ -23,37 +23,51 @@
 
 #Exercícios Práticos:
 #1 Calculadora com Funções 
-def somar(a, b):
-    return a + b
+# def somar(a, b):
+#     return a + b
 
-def subitrair(a, b):
-    return a - b
+# def subitrair(a, b):
+#     return a - b
 
-def multiplicar(a, b):
-    return a * b
+# def multiplicar(a, b):
+#     return a * b
 
-def dividir(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Erro: Divisão por zero!"
+# def dividir(a, b):
+#     if b != 0:
+#         return a / b
+#     else:
+#         return "Erro: Divisão por zero!"
 
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-operacao = input("Escolha a operação (+, -, *, /): ")
+# numero1 = float(input("Digite o primeiro número: "))
+# numero2 = float(input("Digite o segundo número: "))
+# operacao = input("Escolha a operação (+, -, *, /): ")
 
-if operacao == '+':
-    resultado = somar(numero1, numero2)
-elif operacao == '-':
-    resultado = subitrair(numero1, numero2)
-elif operacao == '*':
-    resultado = multiplicar(numero1, numero2)
-elif operacao == '/':
-    resultado = dividir(numero1, numero2)
+# if operacao == '+':
+#     resultado = somar(numero1, numero2)
+# elif operacao == '-':
+#     resultado = subitrair(numero1, numero2)
+# elif operacao == '*':
+#     resultado = multiplicar(numero1, numero2)
+# elif operacao == '/':
+#     resultado = dividir(numero1, numero2)
+# else:
+#     resultado = ("Operação inválida!")
+
+# print("Resultado: ",  resultado)
+
+#2 Função para Verificar Número Primo
+
+def eh_primo(numero):
+    if numero <= 1:
+        return False
+    for i in range(2, numero):
+        if numero % i == 0:
+            return False
+    return True
+
+num = int(input("digite um número inteiro: "))
+
+if eh_primo(num):
+    print(f"{num} é um número primo.")
 else:
-    resultado = ("Operação inválida!")
-
-print("Resultado: ",  resultado)
-
-
-
+    print(f"{num} não é um número primo.")
