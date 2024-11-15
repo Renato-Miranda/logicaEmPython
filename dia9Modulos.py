8338c859080747dcade60b8248d3c3a40a4a05e1
Renato <renatomna@gmail.com>
2024-11-15 20:18:13 -0300

    feat: :art: Instalação da biblioteca requests
    Para treinar com utilizar bibliotecas externas

--- dia9Modulos.py
+++ dia9Modulos.py
@@ -55,25 +55,30 @@
 # print(f"{temperatura_c}°C equivale a {temperatura_f}°F e {temperatura_k}°K")
 
 #2. Jogo de adivinhação usando o módulo random
-import random
+# import random
 
-def jogo_adivinhacao():
-    numero_secreto = random.randint(1, 100)
-    tentativas = 0
+# def jogo_adivinhacao():
+#     numero_secreto = random.randint(1, 100)
+#     tentativas = 0
 
-    print("Bem-vindo ao Jogo de Adivinhação!")
-    print("Tente adivinhar o número entre 1 e 100.")
+#     print("Bem-vindo ao Jogo de Adivinhação!")
+#     print("Tente adivinhar o número entre 1 e 100.")
 
-    while True:
-        palpite = int(input("Digite seu palpite: "))
-        tentativas += 1
+#     while True:
+#         palpite = int(input("Digite seu palpite: "))
+#         tentativas += 1
 
-        if palpite == numero_secreto:
-            print(f"Parabéns! Você acertou em {tentativas} tentativas.")
-            break
-        elif palpite < numero_secreto:
-            print("Muito baixo! tente novamente.")
-        else:
-            print("Muito alto! Tente novamente.")
+#         if palpite == numero_secreto:
+#             print(f"Parabéns! Você acertou em {tentativas} tentativas.")
+#             break
+#         elif palpite < numero_secreto:
+#             print("Muito baixo! tente novamente.")
+#         else:
+#             print("Muito alto! Tente novamente.")
 
-jogo_adivinhacao()
+# jogo_adivinhacao()
+
+import requests
+
+resposta = requests.get('https://api.github.com')
+print(f"Status da resposta: {resposta.status_code}")
