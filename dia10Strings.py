4ff567a70992bacef8726f938985301c520b2146
Renato <renatomna@gmail.com>
2024-11-19 13:22:04 -0300

    feat: :art: operações com Strings
    Dia 10 - Lógica de Programação.

---
+++ dia10Strings.py
@@ -0,0 +1,26 @@
+# #fatiamento de string
+# palavra = "Programação"
+# fatia = palavra[0:7]
+# print(fatia)
+
+# #Comprimento da String
+# texto = "Aprendendo Python"
+# tamanho = len(texto)
+# print(f"O texto em {tamanho} caracteres.")
+
+# #Método format() - inserir valores dentro de uma string
+# idade = 26 
+# mensagem = "eu tenho {} anos.".format(idade)
+# print(mensagem)
+
+# #f-strings são uma versão mais simples do format(). Basta colocar um f antes das aspas e usar chaves {} para inserir as variáveis direto.
+# nome = "Carlos"
+# cidade = "São Paulo"
+# mensagem = f"Meu nome é {nome} e moro em {cidade}."
+# print(mensagem)
+
+###Métodos Comuns de Strings###
+
+#upper() - transforma todos os caracteres em maiúsculas.
+texto = "Bom dia"
+print(texto.upper())
