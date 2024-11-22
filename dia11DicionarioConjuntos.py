528756bc71f7f98596f1c1dc8ef1165de63a9a34
Renato <renatomna@gmail.com>
2024-11-22 19:28:25 -0300

    feat: :art: Exercicio: Criação de agenda Telefonica

--- dia11DicionarioConjuntos.py
+++ dia11DicionarioConjuntos.py
@@ -105,3 +105,47 @@ emails_unicos = set(emails)
 print(emails_unicos)
 
 
+###### EXERCÍCIOS PRÁTICOS ########
+#1. Agenda Telefônica
+
+agenda = {}
+
+def adicionar_contato():
+    nome = input("Nome: ")
+    telefone = input("Telefone: ")
+    agenda[nome] = telefone
+    print("Contato adicionado com sucesso!")
+    
+def buscar_contato():
+    nome = input("Nome do contato a buscar: ")
+    if nome in agenda:
+        print(f"Telefone de {nome}: {agenda[nome]}")
+    else:
+        print("Contato não encotrado.")
+        
+def remover_contato():
+    nome = input("Nome do contato a remover: ")
+    if nome in agenda: 
+        del agenda[nome]
+        print("Contato removido com sucesso!")
+    else: 
+        print("contato não encontrado!")
+
+while True:
+    print("\n1. Adicionar contato")
+    print("2. Buscar contato")
+    print("3. Remover contato")
+    print("4. Sair")
+    opcao = input("Escolha uma opção: ")
+    
+    if opcao == "1":
+        adicionar_contato()
+    elif opcao == "2":
+        buscar_contato()
+    elif opcao == "3":
+        remover_contato()
+    elif opcao == "4":
+        break
+    else: 
+        print("Opção invalida!")
+        
