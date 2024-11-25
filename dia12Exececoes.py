3737948d887251b83854109fe0ae21d99faee778
Renato <renatomna@gmail.com>
2024-11-25 20:07:36 -0300

    feat: :art: Usando o finally para limpeza de recursos

--- dia12Exececoes.py
+++ dia12Exececoes.py
@@ -1,25 +1,47 @@
 #exemplo prático: Tentando abrir um arquivo que pode não existir.
-try:
-    arquivo = open('dados.text', 'r')
+# try:
+#     arquivo = open('dados.text', 'r')
+#     conteudo = arquivo.read()
+# except FileNotFoundError:
+#     print("Erro: O arquivo 'dados.txt' não foi encontrado maninho! Sowwy:(")
+# else:
+#     print("Arquivo lido com sucesso!")
+#     print(conteudo)
+# finally:
+#     print("Operação de leitura de arquivo finalizada!")
+
+# ### Lidando com erros de formar segura: ##
+# #Capturando exceções Espercíficas: 
+# try:
+#     numero = int(input("Digite um número inteiro: "))
+#     resultado = 100 / numero
+# except ValueError:
+#     print("Erro: Entrada inválida. Por favor, digite um número inteiro.") 
+# except ZeroDivisionError:
+#     print("Erro: Divisão por zero não é permitida.")
+# else:
+#     print(f"O Resultado é: {resultado}")
+# finally:
+#     print("Operação de divisão finalizada.")
+    
+### Usando else para cod que não deva gerar exceções 
+# try: 
+#     numero = int(input("Digite um número inteiro: "))
+#     resultado = 100 / numero
+# except (ValueError, ZeroDivisionError) as e:
+#     print(f"Erro: {e}")
+# else:
+#     print(f"O resultado da divisão é: {resultado}")
+
+### Usando finally para limpeza de recursos 
+try: 
+    arquivo = open('dados.txt', 'r')
     conteudo = arquivo.read()
 except FileNotFoundError:
-    print("Erro: O arquivo 'dados.txt' não foi encontrado maninho! Sowwy:(")
+    print("Erro: Arquivo não encontrado.")
 else:
     print("Arquivo lido com sucesso!")
-    print(conteudo)
 finally:
-    print("Operação de leitura de arquivo finalizada!")
-
-### Lidando com erros de formar segura: ##
-#Capturando exceções Espercíficas: 
-try:
-    numero = int(input("Digite um número inteiro: "))
-    resultado = 100 / numero
-except ValueError:
-    print("Erro: Entrada inválida. Por favor, digite um número inteiro.") 
-except ZeroDivisionError:
-    print("Erro: Divisão por zero não é permitida.")
-else:
-    print(f"O Resultado é: {resultado}")
-finally:
-    print("Operação de divisão finalizada.")
+    if 'arquivo' in locals():
+        arquivo.close()
+        print("Arquivo fechado.")
