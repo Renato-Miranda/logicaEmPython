6a1a1e10dd0bbca9c07c04685e549b28bded99c5
Renato <renatomna@gmail.com>
2024-11-25 17:44:08 -0300

    feat: :art: Exercício verficação de palavras únicas

--- dia11DicionarioConjuntos.py
+++ dia11DicionarioConjuntos.py
@@ -108,44 +108,50 @@ print(emails_unicos)
 ###### EXERCÍCIOS PRÁTICOS ########
 #1. Agenda Telefônica
 
-agenda = {}
+# agenda = {}
 
-def adicionar_contato():
-    nome = input("Nome: ")
-    telefone = input("Telefone: ")
-    agenda[nome] = telefone
-    print("Contato adicionado com sucesso!")
+# def adicionar_contato():
+#     nome = input("Nome: ")
+#     telefone = input("Telefone: ")
+#     agenda[nome] = telefone
+#     print("Contato adicionado com sucesso!")
     
-def buscar_contato():
-    nome = input("Nome do contato a buscar: ")
-    if nome in agenda:
-        print(f"Telefone de {nome}: {agenda[nome]}")
-    else:
-        print("Contato não encotrado.")
+# def buscar_contato():
+#     nome = input("Nome do contato a buscar: ")
+#     if nome in agenda:
+#         print(f"Telefone de {nome}: {agenda[nome]}")
+#     else:
+#         print("Contato não encotrado.")
         
-def remover_contato():
-    nome = input("Nome do contato a remover: ")
-    if nome in agenda: 
-        del agenda[nome]
-        print("Contato removido com sucesso!")
-    else: 
-        print("contato não encontrado!")
+# def remover_contato():
+#     nome = input("Nome do contato a remover: ")
+#     if nome in agenda: 
+#         del agenda[nome]
+#         print("Contato removido com sucesso!")
+#     else: 
+#         print("contato não encontrado!")
 
-while True:
-    print("\n1. Adicionar contato")
-    print("2. Buscar contato")
-    print("3. Remover contato")
-    print("4. Sair")
-    opcao = input("Escolha uma opção: ")
+# while True:
+#     print("\n1. Adicionar contato")
+#     print("2. Buscar contato")
+#     print("3. Remover contato")
+#     print("4. Sair")
+#     opcao = input("Escolha uma opção: ")
     
-    if opcao == "1":
-        adicionar_contato()
-    elif opcao == "2":
-        buscar_contato()
-    elif opcao == "3":
-        remover_contato()
-    elif opcao == "4":
-        break
-    else: 
-        print("Opção invalida!")
+#     if opcao == "1":
+#         adicionar_contato()
+#     elif opcao == "2":
+#         buscar_contato()
+#     elif opcao == "3":
+#         remover_contato()
+#     elif opcao == "4":
+#         break
+#     else: 
+#         print("Opção invalida!")
         
+
+#2. Verificação de palavras Únicas
+frase = input("Difite uma frase: ")
+palavras = frase.split()
+palavras_unicas = set(palavras)
+print("Palvras únicas:", palavras_unicas)
