3239c97155242dfe23ceee3d81100e5166168a9b
Renato <renatomna@gmail.com>
2024-11-25 21:05:37 -0300

    feat: :art: Calculadora com tratamento de exceções

--- dia12Exececoes.py
+++ dia12Exececoes.py
@@ -34,14 +34,42 @@
 #     print(f"O resultado da divisão é: {resultado}")
 
 ### Usando finally para limpeza de recursos 
-try: 
-    arquivo = open('dados.txt', 'r')
-    conteudo = arquivo.read()
-except FileNotFoundError:
-    print("Erro: Arquivo não encontrado.")
-else:
-    print("Arquivo lido com sucesso!")
-finally:
-    if 'arquivo' in locals():
-        arquivo.close()
-        print("Arquivo fechado.")
+# try: 
+#     arquivo = open('dados.txt', 'r')
+#     conteudo = arquivo.read()
+# except FileNotFoundError:
+#     print("Erro: Arquivo não encontrado.")
+# else:
+#     print("Arquivo lido com sucesso!")
+# finally:
+#     if 'arquivo' in locals():
+#         arquivo.close()
+#         print("Arquivo fechado.")   
+
+### Exemplos Práticos ###
+#1. Calculadora com tratamento de exceções:
+def calculadora():
+    try:
+        num1 = float(input("Digite o primeiro número: "))
+        operador = input("Digite o operador (+, -, *, /): ")
+        num2 = float(input("Digite o segundo número: "))
+        
+        if operador == '+':
+            resultado = num1 + num2
+        elif operador == '-':
+            resultado = num1 - num2
+        elif operador == '*':
+            resultado = num1 * num2
+        elif operador == '/':
+            resultado = num1 / num2
+        else:
+            raise ValueError("Operador inválido.")
+        
+    except ValueError as ve:
+        print(f"Erro de valor: {ve}")
+    except ZeroDivisionError:
+        print("Erro: Divisão por zero.")
+    else:
+        print(f"O resultado é: {resultado}")
+        
+calculadora()
