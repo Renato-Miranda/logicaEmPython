1217e02bb7d942090b35ec728509a1c38aa875e5
Renato <renatomna@gmail.com>
2024-11-27 14:01:56 -0300

    feat: :art: Manipulando dados de JSON e CSV

--- dia13PersistenciaDados.py
+++ dia13PersistenciaDados.py
@@ -1,34 +1,86 @@
-#Abrir um arquivo para leitura:
-arquivo = open('diario.txt', 'r')
-#lendo arquivo inteiro:
-conteudo = arquivo.read()
-print(conteudo)
+# #Abrir um arquivo para leitura:
+# arquivo = open('diario.txt', 'r')
+# #lendo arquivo inteiro:
+# conteudo = arquivo.read()
+# print(conteudo)
 
-#Lendo linhas individualmente:
-linha = arquivo.readline()
-while linha:
-    print(linha.strip()) #strip() remove os caracteres de nova linha
-    linha = arquivo.readline()
+# #Lendo linhas individualmente:
+# linha = arquivo.readline()
+# while linha:
+#     print(linha.strip()) #strip() remove os caracteres de nova linha
+#     linha = arquivo.readline()
 
-#Lendo todas as linhas em uma lista:
-linhas = arquivo.readlines()
-for linha in linhas:
-    print(linha.strip())
+# #Lendo todas as linhas em uma lista:
+# linhas = arquivo.readlines()
+# for linha in linhas:
+#     print(linha.strip())
 
-# Exemplo de Escrita:
-arquivo = open('saida.txt', 'w')
-arquivo.write('Escrevendo uma linha no arquivo.\n')
-arquivo.write('Escrevendo outra linha.\n')
-arquivo.close()
+# # Exemplo de Escrita:
+# arquivo = open('saida.txt', 'w')
+# arquivo.write('Escrevendo uma linha no arquivo.\n')
+# arquivo.write('Escrevendo outra linha.\n')
+# arquivo.close()
 
-# Exemplo de Anexar:
-arquivo = open('saida.txt', 'a')
-arquivo.write('Esta linha será adicionada ao final do arquivo.\n')
-arquivo.close()
+# # Exemplo de Anexar:
+# arquivo = open('saida.txt', 'a')
+# arquivo.write('Esta linha será adicionada ao final do arquivo.\n')
+# arquivo.close()
 
-## Usando Gerenciador de Contexto with: O uso de with é como ter umafechadura automática que fecha a porta da caixa de armazenamento mesmo que algo dê errado. Ele garante que o arquivo seja fechado autimaticamente após o bloco de código, mesmo se ocorrem erros:
+# ## Usando Gerenciador de Contexto with: O uso de with é como ter umafechadura automática que fecha a porta da caixa de armazenamento mesmo que algo dê errado. Ele garante que o arquivo seja fechado autimaticamente após o bloco de código, mesmo se ocorrem erros:
 
-with open('diario.txt', 'r') as arquivo:
-    conteudo = arquivo.read()
-    print(conteudo)
+# with open('diario.txt', 'r') as arquivo:
+#     conteudo = arquivo.read()
+#     print(conteudo)
+
+# ### Manipulação de Diferentes tipos de Arquivos ###
+# ## Arquivos de Texto ##
+# # Exemplo: lendo um arquivo CSV
+
+# with open('receitas.csv', 'r') as arquivo:
+#     for linha in arquivo:
+#         colunas = linha.strip().split(',')
+#         print(colunas)
+        
+# ## Arquivos Binários ##
+# # Exemplo: Copiando um arquivo de imagem:
+
+# with open('imagem.jpg' 'rb') as origem:
+#     with open('copia_imagem.jpg', 'wb') as destino:
+#         destino.write(origem.read())
+        
+# ## Arquivos JSON ##
+# import json
+
+# dados = {
+#     'nome': 'João',
+#     'idade': 30,
+#     'cidades': ['São Paulo', 'Rio de Janeiro']
+# }
+
+# with open('dados.json', 'w') as arquivo:
+#     json.dump(dados, arquivo, indent=4)
     
+# # Lendo dados de um arquivo JSON
+# import json
+
+# with open('dados.json', 'r') as arquivo:
+#     dados = json.load(arquivo)
+#     print(dados)
+    
+## Escrevendo um arquivo CSV ##
+import csv
+
+with open('contatos.csv', 'w', newline='') as arquivo_csv:
+    escritor = csv.writer(arquivo_csv)
+    escritor.writerow(['Nome', 'Telefone', 'Email'])
+    escritor.writerow(['Ana', '9999-8888', 'ana@exemple.com'])
+    escritor.writerow(['Pedro', '9999-9999', 'pedro@example.com'])
+
+# Lendo um arquivo CSV:
+
+import csv
+
+with open('contatos.csv', 'r') as arquivo_csv:
+    leitor = csv.reader(arquivo_csv)
+    for linha in leitor:
+        print(linha)
