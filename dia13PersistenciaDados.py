cd33524e654ee956af334246fbec3dd0c539dca0
Renato <renatomna@gmail.com>
2024-11-27 14:28:19 -0300

    feat: :art: Exemplos Práticos: Gerenciador de contatos com CSV

--- dia13PersistenciaDados.py
+++ dia13PersistenciaDados.py
@@ -68,43 +68,82 @@
 #     print(dados)
     
 ## Escrevendo um arquivo CSV ##
-import csv
+# import csv
 
-with open('contatos.csv', 'w', newline='') as arquivo_csv:
-    escritor = csv.writer(arquivo_csv)
-    escritor.writerow(['Nome', 'Telefone', 'Email'])
-    escritor.writerow(['Ana', '9999-8888', 'ana@exemple.com'])
-    escritor.writerow(['Pedro', '9999-9999', 'pedro@example.com'])
+# with open('contatos.csv', 'w', newline='') as arquivo_csv:
+#     escritor = csv.writer(arquivo_csv)
+#     escritor.writerow(['Nome', 'Telefone', 'Email'])
+#     escritor.writerow(['Ana', '9999-8888', 'ana@exemple.com'])
+#     escritor.writerow(['Pedro', '9999-9999', 'pedro@example.com'])
 
-# Lendo um arquivo CSV:
+# # Lendo um arquivo CSV:
 
-import csv
+# import csv
 
-with open('contatos.csv', 'r') as arquivo_csv:
-    leitor = csv.reader(arquivo_csv)
-    for linha in leitor:
-        print(linha)
+# with open('contatos.csv', 'r') as arquivo_csv:
+#     leitor = csv.reader(arquivo_csv)
+#     for linha in leitor:
+#         print(linha)
         
 ### Exemplos Práticos ### 
 #1. Contador de Palavras em uma arquivo:
 
-nome_arquivo = input("Digite o nome deo arquivo: ")
+# nome_arquivo = input("Digite o nome deo arquivo: ")
 
-try:
-    with open(nome_arquivo, 'r') as arquivo:
-        conteudo = arquivo.read()
-except FileNotFoundError:
-    print("Erro: Arquivo não encontrado.")
-else:
-    palavras = conteudo.lower().split()
-    contagem = {}
+# try:
+#     with open(nome_arquivo, 'r') as arquivo:
+#         conteudo = arquivo.read()
+# except FileNotFoundError:
+#     print("Erro: Arquivo não encontrado.")
+# else:
+#     palavras = conteudo.lower().split()
+#     contagem = {}
     
-    for palavra in palavras:
-        palavra = palavra.strip('.,!?";:-()')
-        if palavra in contagem:
-            contagem[palavra] += 1
-        else:
-            contagem[palavra] = 1
+#     for palavra in palavras:
+#         palavra = palavra.strip('.,!?";:-()')
+#         if palavra in contagem:
+#             contagem[palavra] += 1
+#         else:
+#             contagem[palavra] = 1
             
-    for palavra, quantidade in contagem.items():
-        print(f"{palavra}: {quantidade}")
+#     for palavra, quantidade in contagem.items():
+#         print(f"{palavra}: {quantidade}")
+        
+#2. Gerenciador de Contatos:
+
+import csv
+
+def adicionar_contato():
+    nome = input("Nome: ")
+    telefone = input("Telefone: ")
+    email = input("Email: ")
+    
+    with open('contato.csv', 'a', newline='') as arquivo_csv:
+        escritor = csv.writer(arquivo_csv)
+        escritor.writerow([nome, telefone, email])
+    print("contato adicionado com sucesso!")
+    
+def listar_contatos():
+    try:
+        with open('contato.csv', 'r',) as arquivo_csv:
+            leitor = csv.reader(arquivo_csv)
+            for linha in leitor:
+                print(f"Nome: {linha[0]}, telefone: {linha[1]}, email: {linha[2]}")
+    except FileNotFoundError:
+        print("Nenhum contato encontrado.")
+        
+while True:
+    print("\nMenu:")
+    print("1. Adicionar Contato")
+    print("2. Listar Contatos")
+    print("3. Sair")
+    opcao = input("Escolha uma opção: ")
+    
+    if opcao == '1':
+        adicionar_contato()
+    elif opcao == '2':
+        listar_contatos()   
+    elif opcao == '3':
+        break
+    else:
+        print("Opção inválida.")
