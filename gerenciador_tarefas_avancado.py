76dee9f49920c9b183ec05f3085b149130329b3c
Renato <renatomna@gmail.com>
2024-11-28 17:33:25 -0300

    feat: :art: Criação das funções para carregar e Salvar Tarefas
    Criação de duas funções: carregar _tarefas e salvar_tarefas

--- gerenciador_tarefas_avancado.py
+++ gerenciador_tarefas_avancado.py
@@ -3,3 +3,15 @@
 import json
 import os
 from datetime import datetime
+# -----------Funções para Carregar e Salvar Tarefas --------
+# Função para carregar as tarefas do arquivo:
+def  carregar_tarefas(tarefas):
+    if os.path.exists('tarefas.json'):
+        with open('tarefas.json', 'r') as arquivo:
+            return json.load(arquivo)
+    else:
+        return []
+# Função para salvar as tarefas no arquivo:
+def salvar_tarefas(tarefas):
+    with open('tarefas.json', 'w') as arquivo:
+        json.dump(tarefas, arquivo, indent=4)
