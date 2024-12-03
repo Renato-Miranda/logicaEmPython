97d510b78ce9cb9e5435d30287d0fd6668a4173f
Renato <renatomna@gmail.com>
2024-12-03 11:39:14 -0300

    feat: :art: Criação da Classe Carro e de seus atributos e métodos
    Foram criadas duas instâncias a partir da classe carro: carro1 e carro2

--- dia15POO.py
+++ dia15POO.py
@@ -1,6 +1,35 @@
 # Sintaxe de Classe:
-class Carro:
+class Carros:
     pass 
 #Criando objetos (instâncias) da classe carro:
-meu_carro = Carro()
-seu_carro = Carro()
+meu_carro = Carros()
+seu_carro = Carros()
+# Exemplo com Atributos e Métodos:
+class Carro:
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca     # Atributo de instancia
+        self.modelo = modelo   # Atributo de instancia
+        self.ano = ano         # Atributo de instancia
+        self.velocidade = 0    # Atributo de instancia
+        
+    def acelerar(self, incremento):
+        self.velocidade += incremento
+        print(f"O carro está agora a {self.velocidade} km/h.")
+    
+    def frear(self, decremento):
+        self.velocidade -= decremento
+        if self.velocidade < 0:
+            self.velocidade = 0
+        print(f"O carro está agora a {self.velocidade} km/h.")
+# Atributos: São como as caracteristicas do carro
+# Métodos: São as ações que o carro pode realizar
+
+## Criando e usando Objetos ##
+#Criando Objetos:
+carro1 = Carro("Toyota", "Corolla", 2020)
+carro2 = Carro("Honda", "Civic", 2018)
+
+#Usando métodos:
+carro1.acelerar(100)
+carro2.acelerar(50)
+carro1.frear(80)
