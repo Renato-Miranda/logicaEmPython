ac3b7741edb032f8ecd7b0b4f2ab874121ed8051
Renato <renatomna@gmail.com>
2024-12-04 18:51:39 -0300

    feat: :art: Criação da Classe Aluno
    Métodos de adicionar notas e calcular média das notas

--- dia15POO.py
+++ dia15POO.py
@@ -88,3 +88,25 @@ class Retangulo:
 retangulo = Retangulo(5, 3)
 print(f"Áreas: {retangulo.area()}")
 print(f"Perímetro: {retangulo.perimetro()}")
+
+#2. Classe Aluno:
+class Aluno:
+    def __init__(self, nome):
+        self.nome = nome
+        self.notas = []
+    
+    def adicionar_nota(self, nota):
+        self.notas.append(nota)
+        
+    def calcular_media(self):
+        if self.notas:
+            return sum(self.notas) / len(self.notas)
+        else:
+            return 0
+        
+aluno = Aluno("Joãozinho")
+aluno.adicionar_nota(8.5)
+aluno.adicionar_nota(7.0)
+aluno.adicionar_nota(9.0)
+media = aluno.calcular_media()
+print(f"Média de {aluno.nome}: {media:.2f} ")
