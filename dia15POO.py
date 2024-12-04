be4c3fcfd886aae3163295b47e59b1a22b7531e0
Renato <renatomna@gmail.com>
2024-12-04 18:22:39 -0300

    feat: :art: Criação da CLasse Pessoa
    Uso de analogias do mundo real para entendimento de Classes e Objetos

--- dia15POO.py
+++ dia15POO.py
@@ -33,3 +33,25 @@ carro2 = Carro("Honda", "Civic", 2018)
 carro1.acelerar(100)
 carro2.acelerar(50)
 carro1.frear(80)
+
+# ------------------ EXEMPLOS COM ANALOGIAS DO MUNDO REAL --------------------------------
+#1. Classe Pessoa:
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+    def apresentar(self):
+        print(f"Olá meu nome é {self.nome} e tenho {self.idade} anos.")
+    
+    def aniversario(self):
+        self.idade += 1 
+        print(f"Feliz aniversário, {self.nome}! Agora você tem {self.idade} anos.")
+        
+joao = Pessoa("João", 28)
+maria = Pessoa("Maria", 25)
+
+joao.apresentar()
+maria.apresentar()
+
+joao.aniversario()
