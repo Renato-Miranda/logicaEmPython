91db6705c921e09de6d286fbd766e9a48ceeed35
Renato <renatomna@gmail.com>
2024-12-05 14:22:00 -0300

    feat: :art: Exercício Prático - Criação Classe Database com Padrão Singleton
    Garantir que apenas uma instância do banco seja criada

--- dia16PPS.py
+++ dia16PPS.py
@@ -1,5 +1,5 @@
-#---------------------- PADRÕES DE PROJETO BÁSICOS -------------------------------------
-#1. Singleton:
+# ---------------------- PADRÕES DE PROJETO BÁSICOS -------------------------------------
+# 1. Singleton:
 #  Definição
 #  Opadrão Singleton garante que uma classe tenha apenas uma única instância e fornece
 #  umponto global de acesso a ela.
@@ -7,21 +7,25 @@
 #  Imagine que você tem um gerente em uma empresa. Só pode haver um gerente, e todos os
 #  funcionários precisam acessar esse único gerente para tomar decisões importantes
 
+from abc import ABC, abstractmethod
+
+
 class Gerente:
     _instancia = None
-    
+
     def __new__(cls):
         if cls._instancia is None:
-            cls._instancia = super(Gerente,cls).__new__(cls)
+            cls._instancia = super(Gerente, cls).__new__(cls)
             return cls._instancia
-        
+
+
 # Testando so Singleton
 gerente1 = Gerente()
 gerente2 = Gerente()
 
 print(gerente1 is gerente2)
 
-#2 Factory Method (Método de Fábrica)
+# 2 Factory Method (Método de Fábrica)
 #  Definição
 # OpadrãoFactoryMethoddefineumainterfaceparacriarobjetos,maspermitequeas
 #  subclassesdecidamqualclasseinstanciar.Elepromoveodesacoplamentoentreacriação
@@ -31,21 +35,23 @@ print(gerente1 is gerente2)
 #  Afábricaforneceummétodoparacriarveículos,mascadatipodeveículotemsuaprópria
 #  implementaçãoespecífica
 
-from abc import ABC, abstractmethod
 
 class Veiculo(ABC):
     @abstractmethod
     def mover(self):
         pass
-    
+
+
 class Carro(Veiculo):
     def mover(self):
         print("O carro está se movendo.")
-        
+
+
 class Moto(Veiculo):
     def mover(self):
         print("A moto está se movendo.")
-        
+
+
 class FabricaVeiculos:
     def criar_veiculos(self, tipo):
         if tipo == "carro":
@@ -54,7 +60,8 @@ class FabricaVeiculos:
             return Moto()
         else:
             raise ValueError("Tipo de veículo desconhecido.")
-        
+
+
 # Testando o Factory Method:
 fabrica = FabricaVeiculos()
 carro = fabrica.criar_veiculos("carro")
@@ -63,7 +70,7 @@ moto = fabrica.criar_veiculos("moto")
 carro.mover()
 moto.mover()
 
-#3. Observer (Observador)
+# 3. Observer (Observador)
 # Definição
 #  OpadrãoObserverdefineumadependênciaum-para-muitosentreobjetos,demodoque
 #  quandoumobjetomudadeestado, todososseusdependentessãonotificadose
@@ -73,31 +80,35 @@ moto.mover()
 #  alteraçõesnohoráriodasaulas.Semprequeháumamudança,oprofessornotificatodosos
 #  alunos,garantindoquetodosestejamatualizados.
 
+
 class Observador:
     def atualizar(self, mensagem):
         pass
-    
+
+
 class Aluno(Observador):
     def __init__(self, nome):
         self.nome = nome
-        
-    def atualizar(self,mensagem):
+
+    def atualizar(self, mensagem):
         print(f"{self.nome} recebeu a mensagem: {mensagem}")
-        
+
+
 class Professor:
     def __init__(self):
         self.observadores = []
-        
+
     def adicionar_observador(self, observador):
         self.observadores.append(observador)
-        
+
     def remover_observador(self, observador):
         self.observadores.remove(observador)
-        
+
     def notificar_observadores(self, mensagem):
         for observador in self.observadores:
             observador.atualizar(mensagem)
-            
+
+
 # Testando O Observer:
 professor = Professor()
 aluno1 = Aluno("Ana")
@@ -107,3 +118,24 @@ professor.adicionar_observador(aluno1)
 professor.adicionar_observador(aluno2)
 
 professor.notificar_observadores(" A aula será á 14h.")
+
+# --------------------------- EXERCÍCIOS PRÁTICOS --------------------
+# 1. implementando o padrão Singleton na Classe Database:
+
+
+class Database:
+    _instancia = None
+
+    def __new__(cls):
+        if cls._instancia is None:
+            cls._instancia = super(Database, cls).__new__(cls)
+            cls._instancia.conexao = "Conexão com banco de dados estabelecida."
+            return cls._instancia
+
+    def conectar(self):
+        print(self.conexao)
+
+
+# Testando Singleton:
+db1 = Database()
+db1.conectar()
