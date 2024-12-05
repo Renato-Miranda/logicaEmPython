cecf4e07722234f4035108d1c3220caa794ea502
Renato <renatomna@gmail.com>
2024-12-05 13:53:48 -0300

    feat: :art: Criação sintaxe do Padrão Factory Method
    gostei bastante desse método, de fato eu já havia tido contato com ele no curso da Resilia

--- dia16PPS.py
+++ dia16PPS.py
@@ -20,3 +20,45 @@ gerente1 = Gerente()
 gerente2 = Gerente()
 
 print(gerente1 is gerente2)
+
+#2 Factory Method (Método de Fábrica)
+#  Definição
+# OpadrãoFactoryMethoddefineumainterfaceparacriarobjetos,maspermitequeas
+#  subclassesdecidamqualclasseinstanciar.Elepromoveodesacoplamentoentreacriação
+#  deobjetoseousodessesobjetos.
+#  Analogia
+#  Penseemumafábricaqueproduzdiferentestiposdeveículos(carros,motos,caminhões).
+#  Afábricaforneceummétodoparacriarveículos,mascadatipodeveículotemsuaprópria
+#  implementaçãoespecífica
+
+from abc import ABC, abstractmethod
+
+class Veiculo(ABC):
+    @abstractmethod
+    def mover(self):
+        pass
+    
+class Carro(Veiculo):
+    def mover(self):
+        print("O carro está se movendo.")
+        
+class Moto(Veiculo):
+    def mover(self):
+        print("A moto está se movendo.")
+        
+class FabricaVeiculos:
+    def criar_veiculos(self, tipo):
+        if tipo == "carro":
+            return Carro()
+        elif tipo == "moto":
+            return Moto()
+        else:
+            raise ValueError("Tipo de veículo desconhecido.")
+        
+# Testando o Factory Method:
+fabrica = FabricaVeiculos()
+carro = fabrica.criar_veiculos("carro")
+moto = fabrica.criar_veiculos("moto")
+
+carro.mover()
+moto.mover()
