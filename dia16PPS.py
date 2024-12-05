dbd98001db0c97cccee00317edaf4b13cba6a79f
Renato <renatomna@gmail.com>
2024-12-05 14:06:20 -0300

    feat: :art: criação de Padrão Observador
    onde temos uma relação um para muitos

--- dia16PPS.py
+++ dia16PPS.py
@@ -62,3 +62,48 @@ moto = fabrica.criar_veiculos("moto")
 
 carro.mover()
 moto.mover()
+
+#3. Observer (Observador)
+# Definição
+#  OpadrãoObserverdefineumadependênciaum-para-muitosentreobjetos,demodoque
+#  quandoumobjetomudadeestado, todososseusdependentessãonotificadose
+#  atualizadosautomaticamente.
+#  Analogia
+#  Imaginequevocêestánaescolaetemumprofessorqueinformaaosalunossobre
+#  alteraçõesnohoráriodasaulas.Semprequeháumamudança,oprofessornotificatodosos
+#  alunos,garantindoquetodosestejamatualizados.
+
+class Observador:
+    def atualizar(self, mensagem):
+        pass
+    
+class Aluno(Observador):
+    def __init__(self, nome):
+        self.nome = nome
+        
+    def atualizar(self,mensagem):
+        print(f"{self.nome} recebeu a mensagem: {mensagem}")
+        
+class Professor:
+    def __init__(self):
+        self.observadores = []
+        
+    def adicionar_observador(self, observador):
+        self.observadores.append(observador)
+        
+    def remover_observador(self, observador):
+        self.observadores.remove(observador)
+        
+    def notificar_observadores(self, mensagem):
+        for observador in self.observadores:
+            observador.atualizar(mensagem)
+            
+# Testando O Observer:
+professor = Professor()
+aluno1 = Aluno("Ana")
+aluno2 = Aluno("Pedro")
+
+professor.adicionar_observador(aluno1)
+professor.adicionar_observador(aluno2)
+
+professor.notificar_observadores(" A aula será á 14h.")
