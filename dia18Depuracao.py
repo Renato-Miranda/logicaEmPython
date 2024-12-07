a958a111536d77a5f2a50cd1ac2b5d2fb76d305b
Renato <renatomna@gmail.com>
2024-12-07 13:03:20 -0300

    feat: :tada: Começo do 18° dia do curso de lógica de programação
    Estudos em debugging e Testes unitários.

---
+++ dia18Depuracao.py
@@ -0,0 +1,4 @@
+#  O Que é Depuração?
+#  Depuração (ou debugging) é o processo de identificar, analisar e corrigir erros ou bugs no
+#  seu código. Mesmo os programadores mais experientes encontram erros, e saber como
+#  depurá-los de forma eficiente é uma habilidade essencial.
