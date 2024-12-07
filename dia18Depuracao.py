9249c15d4878d5a84009a46c3b3c21cd79e4c372
Renato <renatomna@gmail.com>
2024-12-07 13:08:41 -0300

    feat: :art: Exemplo de uso de Print Statements como uma maneira de Depuração.

--- dia18Depuracao.py
+++ dia18Depuracao.py
@@ -2,3 +2,21 @@
 #  Depuração (ou debugging) é o processo de identificar, analisar e corrigir erros ou bugs no
 #  seu código. Mesmo os programadores mais experientes encontram erros, e saber como
 #  depurá-los de forma eficiente é uma habilidade essencial.
+
+# Técnicas de Depuração
+#  1. Leitura Atenta do Código
+#  Antes de tudo, leia seu código cuidadosamente. Às vezes, o erro está em uma linha simples
+#  que passou despercebida.
+#  2. Uso de Print Statements
+#  Adicionar declarações print() em diferentes partes do código ajuda a verificar o fluxo de
+#  execução e os valores das variáveis em tempo real.
+#  Exemplo:
+def dividir(a, b):
+    print(f"Dividindo {a} por {b}")
+    resultado = a / b
+    print(f"Resultado: {resultado}")
+    return resultado
+
+
+dividir(10, 2)
+dividir(5, 0)  # vai causar um erro.
