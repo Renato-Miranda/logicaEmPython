b17ff0c6b3dc5772a8540b28be10f9d8a74d85d3
Renato <renatomna@gmail.com>
2024-12-09 17:44:39 -0300

    feat: :construction: configuração do Logging.

--- biblioteca.py
+++ biblioteca.py
@@ -3,3 +3,6 @@ import json
 import os
 from datetime import datetime
 import logging
+# Configurar Logging:
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
