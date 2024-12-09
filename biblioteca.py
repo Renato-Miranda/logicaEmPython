6639e6820fc909621dd77d916051896e7776f4a1
Renato <renatomna@gmail.com>
2024-12-09 18:02:16 -0300

    feat: :construction: Criação da Classe Emprestimo
    Atributos: id, usuario_id, livro_id, data_emprestimo, data_devolucao. Método: exibir-informacoes.

--- biblioteca.py
+++ biblioteca.py
@@ -18,7 +18,7 @@ class Livro:
         self.isbn = isbn
         self.disponivel = True
 
-    def exibir_informações(self):
+    def exibir_informacoes(self):
         status = "Disponível" if self.disponivel else "Emprestado"
         print(f"ID: {self.id}, Título: {self.titulo}, Autor: {self.autor}, Ano: {self.ano}, Isbn: {self.isbn}, Status: {status}")
 # Classe usuário:
@@ -29,5 +29,17 @@ class Usuario:
         self.email = email
         self.telefone = telefone
     
-    def exibir_informações(self):
+    def exibir_informacoes(self):
         print(f"ID: {self.id}, Nome: {self.nome}, Email: {self.email}, Telefone: {self.telefone}")
+# Classe Emprestimo:
+class Emprestimo:
+    def __init__(self,id, usuario_id, livro_id, data_emprestimo, data_devolucao=None):
+        self.id = id
+        self.usuario_id = usuario_id
+        self.livro_id = livro_id
+        self.data_emprestimo = data_emprestimo
+        self.data_devolucao = data_devolucao
+        
+    def exibir_informacoes(self):
+        status = "Devolvido" if self.data_devolucao else "Pendente"
+        print(f"ID: {self.id}, Usuário ID: {self.usuario_id}, Livro ID: {self.livro_id}, Data Emprestimo: {self.data_emprestimo}, Data Devolução: {self.data_devolucao}, Status: {status}")
