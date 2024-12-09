bd0914646cce70a1d4548b564f73442732a46472
Renato <renatomna@gmail.com>
2024-12-09 17:51:01 -0300

    feat: :construction: Criação da Classe Livro
    Atributos: id, titulo, autor, ano, isbn e método exibir_informações

--- biblioteca.py
+++ biblioteca.py
@@ -6,3 +6,19 @@ import logging
 # Configurar Logging:
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
+# Classe Livro:
+
+
+class Livro:
+    def __init__(self, id, titulo, autor, ano, isbn):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.isbn = isbn
+        self.disponivel = True
+
+    def exibir_informações(self):
+        status = "Disponível" if self.disponivel else "Emprestado"
+        print(f"ID: {self.id}, Título: {self.titulo}, Autor: {
+              self.autor}, Ano: {self.ano}, Isbn: {self.isbn}, Status: {status}")
