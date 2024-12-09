4cb512e7d150db6ad44e2e470a5bb40a8d6c6bca
Renato <renatomna@gmail.com>
2024-12-09 17:55:32 -0300

    feat: :construction: Criação da Classe Usuario
    atributos id, nome, email. telefone e método de exibir_informações.

--- biblioteca.py
+++ biblioteca.py
@@ -20,5 +20,14 @@ class Livro:
 
     def exibir_informações(self):
         status = "Disponível" if self.disponivel else "Emprestado"
-        print(f"ID: {self.id}, Título: {self.titulo}, Autor: {
-              self.autor}, Ano: {self.ano}, Isbn: {self.isbn}, Status: {status}")
+        print(f"ID: {self.id}, Título: {self.titulo}, Autor: {self.autor}, Ano: {self.ano}, Isbn: {self.isbn}, Status: {status}")
+# Classe usuário:
+class Usuario:
+    def __init__(self, id, nome, email, telefone):
+        self.id = id
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+    
+    def exibir_informações(self):
+        print(f"ID: {self.id}, Nome: {self.nome}, Email: {self.email}, Telefone: {self.telefone}")
